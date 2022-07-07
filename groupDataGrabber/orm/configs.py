ab4206b8c242033c36b366f7dbd91cff87107e26
denisbondarenko <last.head@yandex.ru>
2022-07-07 16:29:54 +0300

    t1-shop Add ORM, refactoring the config

--- groupDataGrabber/orm/configs.py
+++ groupDataGrabber/orm/configs.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import settings
 
 load_dotenv()
-env_path = Path('.')/'.env'
+env_path = Path('../../')/'.env'
 load_dotenv(dotenv_path=env_path)
 
 HOSTNAME = os.getenv("HOSTNAME")
